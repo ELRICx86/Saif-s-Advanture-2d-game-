d68769a89b03f3238e83ab6b2a2dbf38249d1645
ELRICx86 <sajidul.saif.71@gmail.com>
2022-06-11 14:13:03 +0600

    player movement and player sprite added

--- src/main/gamePanel.java
+++ src/main/gamePanel.java
@@ -8,13 +8,15 @@ import java.nio.DoubleBuffer;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class gamePanel extends JPanel implements Runnable{
 	
 	//screen settings
 	
 	final int originalTileSize =16;
 	final int scale =3;
-	final int tilesize = originalTileSize * scale; //48x48
+	public final int tilesize = originalTileSize * scale; //48x48
 	
 	final int maxScreenCol =16;
 	final int maxScreenRow=12;
@@ -26,6 +28,8 @@ public class gamePanel extends JPanel implements Runnable{
 	
 	KeyHandler keyH =new KeyHandler();
 	Thread gameThread;
+	
+	Player player =new Player(this, keyH);
 	
 	
 	//set player default position
@@ -85,28 +89,16 @@ public class gamePanel extends JPanel implements Runnable{
 	
 	public void update() {
 		
-		if(keyH.upPressed==true) {
-			playerY-=playerSpeed;
-		}
-		else if (keyH.downPressed==true) {
-			playerY+=playerSpeed;
-		}
-		else if(keyH.leftPressed==true) {
-			playerX-=playerSpeed;
-		}
-		else if (keyH.rightPressed==true) {
-			playerX+=playerSpeed;
-			
-		}
+		player.update();
+		
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2=(Graphics2D)g;
 		
-		g2.setColor(Color.WHITE);
-		
-		g2.fillRect(playerX, playerY, tilesize, tilesize);
+		player.draw(g2);
+
 		g2.dispose();
 		
 		
